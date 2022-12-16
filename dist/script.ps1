3e9b8be209059cf202535d7a0a8070201ade24b5
Kieran Harris <119426175+KieranNewDay@users.noreply.github.com>
2022-12-16 15:28:57 +0000

    Update script.ps1

--- dist/script.ps1
+++ dist/script.ps1
@@ -1,3 +1,3 @@
 Write-Host "Launching Cosmos DB Emulator"
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
-Start-CosmosDbEmulator -EnableGremlinEndpoint
+Start-CosmosDbEmulator -EnableGremlin
